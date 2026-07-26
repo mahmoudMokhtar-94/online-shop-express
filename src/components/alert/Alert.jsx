1cad5bdfd95a1ed651a1e57ad093a74790241653
mahmoudMokhtar-94 <mahmoudformal94@gmail.com>
2026-07-26 11:20:40 +0300

    Fixed responsive issues related to the delete alert dialog

--- src/components/alert/Alert.jsx
+++ src/components/alert/Alert.jsx
@@ -17,21 +17,21 @@ export default function Alert({
   }
   return open ? (
     <div className="fixed inset-0 bg-black/40 backdrop-blur-[2px] z-50 flex items-center justify-center">
-      <div className="w-[40%] mx-auto shadow-[var(--color_heading)] bg-white  p-4">
+      <div className="w-[65%] md:w-[56%] mx-auto shadow-[var(--color_heading)] bg-white  p-4">
         <h2 className="text-[22px] font-bold text-[var(--main_color)] mb-2">
           {title}
         </h2>
         <p className="text-[var(--p_color)]">{message}</p>
-        <div className="flex gap-4 items-center mt-3">
+        <div className="flex justify-center gap-4 flex-col-reverse md:flex-row items-center mt-3">
           <button
             onClick={handleDeleteConfirm}
-            className="flex  gap-2 items-center capitalize bg-[#fee2e2] text-[#991b1b] py-2 px-4 font-bold cursor-pointer duration-300 hover:bg-[#991b1b] hover:text-[#fee2e2]"
+            className="flex  gap-2 items-center capitalize bg-[#fee2e2] text-[#991b1b] py-2 px-4 font-bold cursor-pointer duration-300 hover:bg-[#991b1b] hover:text-[#fee2e2] w-[141px]"
           >
             <Trash /> <span>remove</span>
           </button>
           <button
             onClick={() => dismissAlert()}
-            className="flex  gap-2 items-center capitalize bg-[#dcfce7] text-[#166534] py-2 px-4 font-bold cursor-pointer duration-300 hover:bg-[#166534] hover:text-[#dcfce7]"
+            className="flex  gap-2 items-center capitalize bg-[#dcfce7] text-[#166534] py-2 px-4 font-bold cursor-pointer duration-300 hover:bg-[#166534] hover:text-[#dcfce7] w-[141px]"
           >
             <Undo /> <span>keep item</span>
           </button>
